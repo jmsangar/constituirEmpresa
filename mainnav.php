b85c5e62b2968cd858ff92f7018c2fdf46d7083d
José Manuel Sánchez Garcia <jmansangar@gmail.com>
2014-10-10 10:59:53 +0200

    Se agrega navegacion general main Nav

---
+++ mainnav.php
@@ -0,0 +1,8 @@
+	
+	<ul>
+      <li><a href="http://cevillalba.com/constituirempresa/?tag=septiembre-2014">Inicio</a></li>
+      <li><a href="http://cevillalba.com/constituirempresa/?page_id=189">Quienes somos</a></li>
+      <li><a href="http://cevillalba.com/constituirempresa/?tag=servicios">Servicios</a></li>
+      <li><a href="http://cevillalba.com/constituirempresa/?page_id=116">Situacion</a></li>
+     
+    </ul>
