06f73bd7dbe0fe29af4ef7ea776d5a46ac34ec49
José Manuel Sánchez Garcia <jmansangar@gmail.com>
2014-11-25 20:27:28 +0100

    cambios en el single

--- single.php
+++ single.php
@@ -7,7 +7,9 @@
     
      
     <h1><?php the_post();?>&nbsp;
-    <?php the_title();?></h1>
+    <?php the_title();?></h1> <small>
+                    Por <?php the_author(', '); ?> el <?php the_time('F jS, Y'); ?> en <?php the_category(', ') ?> | <?php comments_popup_link('No Comments »', '1 Comment »', '% Comments »'); ?>
+                </small>
     
      
       <p><?php the_content(); ?></p>
